f18500ab211faba2ba28c581803dce2f5b3ff914
Robert Keller <robert_keller44@icloud.com>
2020-01-21 17:51:37 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,36 @@
+require "pry"
+require "yaml"
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticon_hash = YAML.load_file(path)
+  modified_hash = {
+    "get_meaning": {},
+    "get_emoticon": {}
+  }
+  emoticon_hash.each do |name, both_emoticons|
+    modified_hash[:get_meaning][both_emoticons[1]] = name  # japanese emoticon is the key value should be the meaning
+    modified_hash[:get_emoticon][both_emoticons[0]] =  both_emoticons[1]
+end
+modified_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, english_emoticon)
+  comparison_hash = load_library(file_path)
+   comparison_hash[:get_emoticon].each do |english, japanese|
+      if english == english_emoticon
+      return japanese
+   end
+ end
+ return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, japanese_emoticon)
+comparison_hash = load_library(file_path)
+comparison_hash[:get_meaning].each do |japanese, meaning|
+  if japanese == japanese_emoticon 
+    return meaning
+    end
+  end
+  return "Sorry, that emoticon was not found"
 end
